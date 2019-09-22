7fca98151dd3103d8f003ca56d487e41348394ef
Vennela Vysyaraju <vvysyaraju@horizon.csueastbay.edu>
2019-09-21 19:19:20 -0700

    completed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var app = express.Router();
+var mongodb= require('mongodb');
 
 /* GET home page. */
 
@@ -13,23 +14,23 @@ module.exports = router;
 //**************************************************************************
 //***** mongodb get all of the Routes in Routes collection where frequence>=1
 //      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
-app.get('/mongodb', function (request, response) {
-                             
-     mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
-         if(err) throw err;
-          //get collection of routes
-          var Routes = db.collection('Routes');
-          //get all Routes with frequency >=1
-          Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
-              if(err) throw err;
+router.get('/mongodb', function (request, response) {
 
-              response.render('pages/mongodb', {results: docs});
-   
-          });
-   
-           //close connection when your app is terminating.
-            db.close(function (err) {
-                     if(err) throw err;
-             });
- });//end of connect
+    mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+        if(err) throw err;
+        //get collection of routes
+        var Routes = db.collection('Routes');
+        //get all Routes with frequency >=1
+        Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('pages/mongodb', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
 });//end app.get
